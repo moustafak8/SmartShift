ba62c388fccfaf9120db72872f924e276954b4ed
moustafak8 <moustafaalkamel@gmail.com>
2026-01-02 22:47:10 +0200

    added the navigation component

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Container } from "./Container";
+export { Card } from "./Card";
+export { Button } from "./Button";
+export { Heading } from "./Heading";
+export { IconText } from "./IconText";
+export { SectionHeader } from "./SectionHeader";
