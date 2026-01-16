42654cb1577141020be2960511df5e695bfbbeda
moustafak8 <moustafaalkamel@gmail.com>
2026-01-16 23:13:43 +0200

    Assigned endpoints to each role

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -16,50 +16,49 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
-    Route::get('employees/{departmentId}', [EmployeeDepartmentController::class, 'getemployees']);
-    Route::get('shifts/{departmentId}', [ShiftsController::class, 'getShifts']);
-    Route::post('shifts', [ShiftsController::class, 'createShift']);
-    Route::get('shift-templates', [ShiftTemplatesController::class, 'getTemplates']);
     Route::post('shift-templates', [ShiftTemplatesController::class, 'createTemplate']);
-    Route::get('shift-assignments', [ShiftAssigmentsController::class, 'getAssignments']);
-    Route::get('shifts/{id}/assignments', [ShiftAssigmentsController::class, 'getShiftAssignments']);
-    Route::post('shift-assignments', [ShiftAssigmentsController::class, 'createAssignment']);
-    Route::post('shift-assignments/bulk', [ShiftAssigmentsController::class, 'createBulkAssignments']);
-    Route::put('shift-assignments/{assignmentId}', [ShiftAssigmentsController::class, 'updateAssignment']);
-    Route::delete('shift-assignments/{assignmentId}', [ShiftAssigmentsController::class, 'deleteAssignment']);
-    Route::get('shift-assignments/week', [ShiftAssigmentsController::class, 'getWeeklyAssignments']);
     Route::get('employees/{employeeId}/shift-assignments/week', [ShiftAssigmentsController::class, 'getWeeklyAssignmentsByEmployee']);
-    Route::get('wellness-entries/{departmentId}', [WellnessEntriesController::class, 'getEntries']);
-    Route::get('employees/{id}/wellness-entries', [WellnessEntriesController::class, 'getByEmployee']);
-    Route::post('wellness-entries', [WellnessEntriesController::class, 'storeEntry']);
     Route::post('wellness/search', [WellnessSearchController::class, 'search']);
-    Route::post('wellness/search/insights', [WellnessSearchController::class, 'searchWithInsights']);
-    Route::get('employee/{id}/shifts', [ShiftsController::class, 'getEmployeeShifts']);
-    Route::get('/fatigue-scores/{employeeId}', [FatigueScoreController::class, 'getEmployeeScore']);
-    Route::get('employee-preferences', [EmployeePreferencesController::class, 'listPreferences']);
-    Route::get('employees/{employeeId}/preferences', [EmployeePreferencesController::class, 'getPreference']);
-    Route::post('employee-preferences', [EmployeePreferencesController::class, 'storePreference']);
-    Route::delete('employees/{employeeId}/preferences', [EmployeePreferencesController::class, 'deletePreference']);
-    Route::get('employees/{employeeId}/availability', [EmployeeAvailabilityController::class, 'getAvailability']);
-    Route::get('departments/{departmentId}/available-employees', [EmployeeAvailabilityController::class, 'getAvailableEmployeesForDate']);
-    Route::post('employee-availability', [EmployeeAvailabilityController::class, 'storeAvailability']);
-    Route::put('employee-availability/{id}', [EmployeeAvailabilityController::class, 'updateAvailability']);
-    Route::delete('employee-availability/{id}', [EmployeeAvailabilityController::class, 'deleteAvailability']);
-    Route::post('schedules/generate', [GenerateScheduleController::class, 'generate']);
-    Route::post('schedules/save-reviewed', [GenerateScheduleController::class, 'saveReviewed']);
     Route::get('positions', [PositionController::class, 'getAllPositions']);
-    Route::get('departments/{departmentId}/positions', [PositionController::class, 'getPositionsByDepartment']);
-    Route::get('positions/{positionId}', [PositionController::class, 'getPositionById']);
     Route::post('positions', [PositionController::class, 'storePosition']);
     Route::put('positions/{positionId}', [PositionController::class, 'updatePosition']);
     Route::delete('positions/{positionId}', [PositionController::class, 'deletePosition']);
     Route::post('logout', [AuthController::class, 'logout'])->middleware('jwt');
     Route::get('me', [AuthController::class, 'me'])->middleware('jwt');
 
-    Route::prefix('manager')->middleware('manager')->group(function () {
+    Route::prefix('')->middleware('manager')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
+        Route::get('employees/{departmentId}', [EmployeeDepartmentController::class, 'getemployees']);
+        Route::get('shifts/{departmentId}', [ShiftsController::class, 'getShifts']);
+        Route::post('shifts', [ShiftsController::class, 'createShift']);
+        Route::get('shift-templates', [ShiftTemplatesController::class, 'getTemplates']);
+        Route::post('shift-assignments', [ShiftAssigmentsController::class, 'createAssignment']);
+        Route::post('shift-assignments/bulk', [ShiftAssigmentsController::class, 'createBulkAssignments']);
+        Route::put('shift-assignments/{assignmentId}', [ShiftAssigmentsController::class, 'updateAssignment']);
+        Route::delete('shift-assignments/{assignmentId}', [ShiftAssigmentsController::class, 'deleteAssignment']);
+        Route::get('shift-assignments', [ShiftAssigmentsController::class, 'getAssignments']);
+        Route::get('shifts/{id}/assignments', [ShiftAssigmentsController::class, 'getShiftAssignments']);
+        Route::get('shift-assignments/week', [ShiftAssigmentsController::class, 'getWeeklyAssignments']);
+        Route::get('wellness-entries/{departmentId}', [WellnessEntriesController::class, 'getEntries']);
+        Route::post('wellness/search/insights', [WellnessSearchController::class, 'searchWithInsights']);
+        Route::get('departments/{departmentId}/available-employees', [EmployeeAvailabilityController::class, 'getAvailableEmployeesForDate']);
+        Route::post('schedules/generate', [GenerateScheduleController::class, 'generate']);
+        Route::post('schedules/save-reviewed', [GenerateScheduleController::class, 'saveReviewed']);
+        Route::get('departments/{departmentId}/positions', [PositionController::class, 'getPositionsByDepartment']);
+        Route::get('positions/{positionId}', [PositionController::class, 'getPositionById']);
     });
-    Route::prefix('employee')->middleware('employee')->group(function () {
-        // Employee-specific routes go here
+    Route::prefix('')->middleware('employee')->group(function () {
+        Route::post('wellness-entries', [WellnessEntriesController::class, 'storeEntry']);
+        Route::get('employees/{id}/wellness-entries', [WellnessEntriesController::class, 'getByEmployee']);
+        Route::get('employee/{id}/shifts', [ShiftsController::class, 'getEmployeeShifts']);
+        Route::get('/fatigue-scores/{employeeId}', [FatigueScoreController::class, 'getEmployeeScore']);
+        Route::get('employee-preferences', [EmployeePreferencesController::class, 'listPreferences']);
+        Route::get('employees/{employeeId}/preferences', [EmployeePreferencesController::class, 'getPreference']);
+        Route::post('employee-preferences', [EmployeePreferencesController::class, 'storePreference']);
+        Route::delete('employees/{employeeId}/preferences', [EmployeePreferencesController::class, 'deletePreference']);
+        Route::get('employees/{employeeId}/availability', [EmployeeAvailabilityController::class, 'getAvailability']);
+        Route::post('employee-availability', [EmployeeAvailabilityController::class, 'storeAvailability']);
+        Route::put('employee-availability/{id}', [EmployeeAvailabilityController::class, 'updateAvailability']);
+        Route::delete('employee-availability/{id}', [EmployeeAvailabilityController::class, 'deleteAvailability']);
     });
 });
